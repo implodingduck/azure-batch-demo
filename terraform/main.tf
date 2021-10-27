aa9a2c11388b05c1fa56cb640a7e7eb70220857b
implodingduck <zsek320@gmail.com>
2021-10-26 20:47:30 -0500

    fixing references

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,8 +45,8 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_storage_account" "sa" {
   name                     = "satrigger${local.func_name}"
-  resource_group_name      = var.resource_group_name
-  location                 = var.resource_group_location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
